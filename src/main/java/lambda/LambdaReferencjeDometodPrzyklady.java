bf7d6760f6023c553ba0a3a275b6f6c99a62f047
Tomasz Konieczek <tomekkonieczek@gmail.com>
2020-06-28 13:12:26 +0200

    Lamda referencje do metod

---
+++ src/main/java/lambda/LambdaReferencjeDometodPrzyklady.java
@@ -0,0 +1,83 @@
+package lambda;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
+
+public class LambdaReferencjeDometodPrzyklady {
+
+    public static void main(String[] args) {
+        // 1) Klasa::metodaStatyczna
+        // 2) obiekt::metodaInstacji
+        // 3) Klasa::metodaInstancji
+        // 4) Referencje do konstruktora
+
+        List<String> surNames = new ArrayList<>();
+        surNames.add("Kowalski");
+        surNames.add("Kot");
+        surNames.add("Pies");
+
+        // 1) Klasa::metodaStatyczna
+        // Wszystkie parametry są przekazywane metodzie statycznej
+        surNames.forEach(surname -> System.out.println(surname));
+        surNames.forEach(System.out::println);
+
+        surNames.forEach(surname -> metodka(surname));
+        surNames.forEach(LambdaReferencjeDometodPrzyklady::metodka);
+
+        surNames.stream().filter(str -> Objects.isNull(str));
+        surNames.stream().filter(Objects::isNull);
+
+
+
+        List<String> nazwyProduktow = new ArrayList<>();
+        nazwyProduktow.add("Mleko");
+        nazwyProduktow.add("Maslo");
+        nazwyProduktow.add("Chleb");
+
+        // 2) obiekt::metodaInstacji
+        // Wszystkie parametry są przekazywane metodyInstancji
+        Product p = new Product("Kawa");
+        nazwyProduktow.forEach(nazwaProduktu -> p.czyJestemProduktem(nazwaProduktu));
+        nazwyProduktow.forEach(p::czyJestemProduktem);
+
+        // Pierwszy parametr staje się odbiorcą metody i wszystkie inne parametry są przekazywane do metody.
+        // 3) Klasa::metodaInstancji
+        List<String> produktyDuzymiLiterami = nazwyProduktow.stream().map(nazwaProduktu -> nazwaProduktu.toUpperCase()).collect(Collectors.toList());
+        List<String> produktyDuzymiLiterami2 = nazwyProduktow.stream().map(String::toUpperCase).collect(Collectors.toList());
+
+        String max1 = nazwyProduktow.stream().max((np1, np2) -> np1.compareTo(np2)).orElse("");
+        String max2 = nazwyProduktow.stream().max(String::compareTo).orElse("");
+
+
+        // 4) Referencje do konstruktora
+        List<Product> collect = nazwyProduktow.stream().map(nazwaProduktu -> new Product(nazwaProduktu)).collect(Collectors.toList());
+        List<Product> collect1 = nazwyProduktow.stream().map(Product::new).collect(Collectors.toList());
+
+    }
+
+    private static int metodka(String s) {
+        System.out.println(s + " " + s);
+        return 0;
+    }
+
+
+
+    // 2) obiekt::metodaInstacji
+    public void metodaInstacji(String s) {
+        List<String> produkty = new ArrayList<>();
+        produkty.add("Mleko");
+        produkty.add("Maslo");
+        produkty.add("Chleb");
+
+
+        produkty.forEach(nazwaProduktu -> this.superMetoda(nazwaProduktu));
+        produkty.forEach(this::superMetoda);
+    }
+
+    public void superMetoda(String s) {
+        System.out.println(s);
+    }
+
+}
---
+++ src/main/java/lambda/Product.java
@@ -0,0 +1,15 @@
+package lambda;
+
+public class Product {
+
+    String name;
+
+    public Product(String name) {
+        this.name = name;
+    }
+
+
+    public void czyJestemProduktem(String s) {
+
+    }
+}
